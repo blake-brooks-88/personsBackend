a3d3d434b69c0518404c18708ac3d8d3821099e1
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2025-05-07 16:11:05 -0500

    remove requirement for certain number of args

--- mongo.js
+++ mongo.js
@@ -5,8 +5,7 @@ if (process.argv.length < 3) {
     process.exit(1)
 }
 
-const password = process.argv[2]
-const url = `mongodb+srv://fullstack:${password}@fullstack.leoei1o.mongodb.net/personApp?retryWrites=true&w=majority&appName=fullstack`
+const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
